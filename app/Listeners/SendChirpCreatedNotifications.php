a591e85c1d87a398adb670607b83b9884cd92553
Gilberto da Silva Batista <gilberto.batista@crefaz.com.br>
2024-12-08 18:22:56 -0300

    feat: Email notification when creating a message.

---
+++ app/Listeners/SendChirpCreatedNotifications.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\ChirpCreated;
+use App\Models\User;
+use App\Notifications\NewChirp;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\Log;
+
+class SendChirpCreatedNotifications implements ShouldQueue
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(ChirpCreated $event)
+    {
+        Log::info('ChirpCreated event handled', ['chirp_id' => $event->chirp->id]);
+        foreach (User::whereNot('id', $event->chirp->user_id)->cursor() as $user) {
+            $user->notify(new NewChirp($event->chirp));
+        }
+    }
+}
---
+++ app/Notifications/NewChirp.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Models\User;
+use App\Models\Chirp;
+use Illuminate\Support\Str;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Notification;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class NewChirp extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct(public Chirp $chirp)
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        if (!$notifiable instanceof User) {
+            throw new \InvalidArgumentException('Notifiable must be an instance of User');
+        }
+
+        $mailMessage = (new MailMessage)
+            ->subject("New Chirp from {$this->chirp->user->name}")
+            ->greeting("New Chirp from {$this->chirp->user->name}")
+            ->line(Str::limit($this->chirp->message, 50))
+            ->action('Go to Chirper', url('/'))
+            ->line('Thank you for using our application!');
+
+        return $mailMessage;
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            'chirp_id' => $this->chirp->id,
+            'user_name' => $this->chirp->user->name,
+            'message' => Str::limit($this->chirp->message, 50),
+        ];
+    }
+}
